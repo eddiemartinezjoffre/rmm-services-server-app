db6ef1273d893d7d2645be4417334b6b1c6a265c
eddiemartinezjoffre <eddie.martinez.joffre@outlook.com>
2022-01-25 22:00:51 -0500

    Working on Authentication

---
+++ src/main/java/com/ninja/rmm/security/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.ninja.rmm.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("admin");
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/customers/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/customers").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.PUT, "/customers/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE, "/customers/**").hasRole("ADMIN");
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic().and().authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
